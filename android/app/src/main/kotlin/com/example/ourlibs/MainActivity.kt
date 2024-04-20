f21678543968b13b9b67efdb2a86e63fbb7719e5
delandapp <alihandronikolas@gmail.com>
2024-04-20 12:44:38 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ourlibs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ourlibs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/dashboard/bindings/dashboard_binding.dart
@@ -0,0 +1,28 @@
+import 'package:ourlibs/app/modules/bookmark/controllers/bookmark_controller.dart';
+import 'package:ourlibs/app/modules/history/controllers/history_controller.dart';
+import 'package:ourlibs/app/modules/home/controllers/home_controller.dart';
+import 'package:ourlibs/app/modules/profile/controllers/profile_controller.dart';
+import 'package:get/get.dart';
+
+import '../controllers/dashboard_controller.dart';
+
+class DashboardBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<DashboardController>(
+      () => DashboardController(),
+    );
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+    Get.lazyPut<HistoryController>(
+      () => HistoryController(),
+    );
+    Get.lazyPut<BookmarkController>(
+      () => BookmarkController(),
+    );
+    Get.lazyPut<ProfileController>(
+      () => ProfileController(),
+    );
+  }
+}
